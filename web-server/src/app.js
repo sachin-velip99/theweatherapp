1324fae56fef1684ee179dab8c3d8787cf1639d2
Sachin <velipsachin999@gmail.com>
2022-09-24 11:31:59 +0530

    commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 //console.log(__dirname)
 //console.log(path.join(__dirname, '../public'))
@@ -51,10 +53,36 @@ app.get('/help', (req,res) => {
 
 //serving html json 
 app.get('/weather', (req,res) => {
-    res.send({
-        forecast: 'its windy',
-        location: 'goa'
+    if(!req.query.address){
+        return res.send({
+            error: 'you must provide an address'
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location} ={} ) => {
+        if(error){
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
+    
+    
+    /*res.send({
+        forecast: 'its windy',
+        location: 'goa',
+        address: req.query.address
+    })*/
 })
 
 app.get('/help/*', (req,res) => {
